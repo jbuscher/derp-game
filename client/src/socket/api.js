14d7df55743e91a783f9d027b4ac489860518643
John Buscher <john.buscher12@gmail.com>
2018-08-24 17:27:29 -0700

    prod ready for realz

--- client/src/socket/api.js
+++ client/src/socket/api.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client';
 const port = process.env.PORT || 5000;
 const url = process.env.NODE_ENV === 'production' ? 'https://derp-game.herokuapp.com:' : 'http://localhost:'
-const socket = openSocket('http://localhost:' + port);
+const socket = openSocket(url + port);
 
 function subscribeToGameState(cb) {
     socket.on('GameState', gameState => cb(null, gameState));
