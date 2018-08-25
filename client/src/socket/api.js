0f6fbe55c5256073b3f8c9a4cf256f3cbc07becb
John Buscher <john.buscher12@gmail.com>
2018-08-25 11:07:46 -0700

    Added correct url checker for socket

--- client/src/socket/api.js
+++ client/src/socket/api.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client';
 const port = process.env.PORT || 5000;
-const url = process.env.NODE_ENV === 'production' ? 'https://derp-game.herokuapp.com:' : 'http://localhost:'
-const socket = openSocket(url + port);
+const socketurl = window.location.href.includes('localhost') ? 'http://localhost:' : 'https://derp-game.herokuapp.com:'
+const socket = openSocket('http://localhost:' + port);
 
 function subscribeToGameState(cb) {
     socket.on('GameState', gameState => cb(null, gameState));
