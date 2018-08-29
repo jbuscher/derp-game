0522bc3fdf002cd7cc107a082bb3b8c30eda2baa
John Buscher <john.buscher12@gmail.com>
2018-08-28 17:03:48 -0700

    location props for skeletons

--- client/src/components/FightArea.js
+++ client/src/components/FightArea.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import Skeleton from './Skeleton';
+import { relative } from 'path';
 
 export default class FightArea extends Component  {
     render() {
         return (
             <div style={playAreaStyles} >
+                {this.renderPlayGrid()}
                 {this.renderUnits()}
             </div>
         );
     }
 
     renderPlayGrid() {
-
+        return <canvas style={canvasStyle}></canvas>
     }
 
     renderUnits() {
         var skeletons = [];
         for (var i = 0; i < this.props.units; i++) {
-          skeletons.push(<Skeleton key={i}/>);
+          skeletons.push(<Skeleton key={i} squareSize={30} x={i} y={i}/>);
         }
         return skeletons;
     }
 }
 
-var playAreaStyles = {
+const playAreaStyles = {
+    position: 'relative',
     height: 600,
     width: 1200,
     border: 'solid black 1px',
     marginLeft: 'auto',
     marginRight: 'auto',
 };
+
+const canvasStyle = {
+    height: 600,
+    width: 1200,
+}
--- client/src/components/Skeleton.js
+++ client/src/components/Skeleton.js
@@ -3,8 +3,20 @@ import idleSkelton from '../resources/Skeleton/GIFS/Skeleton Idle.gif'
 
 export default class Skeleton extends Component {
     render() {
-        return <div>
-            <img alt='skeleton!' style={{height: 32}} src={idleSkelton} />
+        return <div style={this.calculatePositionStyle()}>
+            <img alt='skeleton!' style={skeletonStyles} src={idleSkelton} />
         </div>;
     }
+
+    calculatePositionStyle() {
+        return {
+            position: 'absolute',
+            top: this.props.squareSize * this.props.y,
+            left: this.props.squareSize * this.props.x,
+        }
+    }
+}
+
+var skeletonStyles = {
+    height: 30
 }
